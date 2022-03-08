16ea4b201e528c5e02a103aee81a600c7b1c2e02
shlifedev <shlifedev@gmail.com>
2022-03-09 02:24:27 +0900

    순환참조 이슈커멘트

--- src/Assets/Script/BoardGame.multiplay/Network/messages/creategame.ts
+++ src/Assets/Script/BoardGame.multiplay/Network/messages/creategame.ts
@@ -15,6 +15,8 @@ export default class CreateGame extends Protocol {
             const player = getPlayer(client);
             const table = Server.Instance.state.tables.get(req.tableId.toString());
             addPlayerToTable(table, player);  
+
+            // 방 생성 알림에 대한 메세지생성
             let notifyMsg: NotifyCreateGame = {
                 onwerSessionId: player.sessionId,
                 table: table.tableId,
--- src/Assets/Script/BoardGame.multiplay/server.ts
+++ src/Assets/Script/BoardGame.multiplay/server.ts
@@ -1,5 +1,13 @@
 /* import시 싱글턴 순환 참조를 피하기 위해서 만들었다. */
+/* 
+ case ->
 
+ index.ts creategame.ts를 import 한 후
+ creategame.ts 에서도 index.ts에 있는 싱글톤을 사용하는경우 index.ts를 import 하게되면
+ 서로간의 순환참조가 되어버리므로 순환참조 오류가 나서 이렇게 따로 싱글턴용
+ 스크립트를 작성했다. 나중에 이 구조를 수정할 수 있는 방법을 찾아야 할듯
+
+*/
 import { Sandbox } from "ZEPETO.Multiplay";
 
 export default class Server{
