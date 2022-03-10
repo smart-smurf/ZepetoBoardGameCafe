e56309129e3c3711545d4322c47567616cd6b38e
Cheese Allergy Hamster <shlifedev@gmail.com>
2022-03-11 01:12:54 +0900

    service create

--- boardgame/Assets/Server.multiplay/Network/game/blackjack/game.ts
+++ boardgame/Assets/Server.multiplay/Network/game/blackjack/game.ts
@@ -1,35 +1,16 @@
-import { SandboxPlayer } from "ZEPETO.Multiplay";
-import { GameTableState } from "../../../Common/Enums";
+import { SandboxPlayer } from "ZEPETO.Multiplay"; 
 import { getPlayer } from "../../service/player";
+import { addPlayerToTable, removePlayerOnTable } from "../../service/table";
 import { GameBase } from "../gamebase";
 
 export class Game extends GameBase {
     public OnPlayerLeave(client: SandboxPlayer): void 
     {
-        if(this.gameTable.status == GameTableState.READY){
-            const player = getPlayer(client);
-            for(let i = 0; i< this.gameTable.players.length; i++){
-                if(this.gameTable.players[i] === player){
-                    this.gameTable.players.deleteAt(i);
-                    break;
-                }
-            } 
-        } 
+        removePlayerOnTable(this.gameTable, getPlayer(client));
     }
 
     public OnPlayerJoin(client: SandboxPlayer): void 
     { 
-        if(this.gameTable.status == GameTableState.READY){
-            const player = getPlayer(client);
-            this.gameTable.players.push(player);
-        } 
-    }
-
-
-
-
-
-
-
-
+        addPlayerToTable(this.gameTable, getPlayer(client));
+    }  
 }
--- boardgame/Assets/Server.multiplay/Network/service/table.ts
+++ boardgame/Assets/Server.multiplay/Network/service/table.ts
@@ -1,40 +1,59 @@
 import { GameTable, Player } from "ZEPETO.Multiplay.Schema";
 import Server from "../../server";
-import { GameTableState, GameType } from "../../Common/Enums"; 
+import { GameTableState, GameType } from "../../Common/Enums";
 
- 
+
 /**
  * 테이블 에서 플레이어 퇴장
  */
-export function removePlayerOnTable(player: Player) {
-    if (player.currentTableId !== 0) {
-        const table = Server.Instance.state.tables.get(player.currentTableId.toString());
-        table.players = table.players.filter(x => x.sessionId !== player.sessionId);
+export function removePlayerOnTable(gameTable: GameTable, player: Player) {
+    const table = Server.Instance.state.tables.get(player.currentTableId.toString());
+    if(table !== gameTable){
+        throw new Error("Valid Failed :: 플레이어의 방과, removePlayerOnTable 인자의 방이 서로 다름.")
+    }
+
+    for (let i = 0; i < table.players.length; i++) {
+        if (table.players[i] === player) {
+            table.players.deleteAt(i);
+            player.currentTableId = 0;
+            // 방장 변경
+            if(table.players.length === 0){
+                table.owner = null;
+            }
+            else{
+                table.owner = table.players[0];
+            }
+            break;
+        }
     }
 }
+ 
 
 /**
 * 플레이어를 테이블에 추가
 */
 export function addPlayerToTable(gameTable: GameTable, player: Player) {
     // 게임 플레이 상태확인
-    if (gameTable.status === 0) {
+    if (gameTable.status === GameTableState.READY) { // 준비 상태인지 확인
         // 방 정원초과 확인
-        if (gameTable.players.length < gameTable.maxPlayer) {
-            gameTable.owner = player;
+        if (gameTable.players.length < gameTable.maxPlayer) { // 정원초과 확인
+            if (gameTable.owner === null) {
+                gameTable.owner = player;
+            }
             gameTable.players.push(player);
-            player.currentTableId = gameTable.tableId; 
+            player.currentTableId = gameTable.tableId;
         }
-        else{
+        else {
             throw new Error("정원초과 테스트 exception");
         }
     }
     else {
+        throw new Error("게임 중간에 참여할 수 없음.");
     }
 }
 
 
-export function createGameInstance(count: number) { 
+export function createGameInstance(count: number) {
     /* 블랙잭 테이블 인스턴스 생성 */
     for (let i = 0; i < count; i++) {
         const table = new GameTable();
